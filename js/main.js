ab2545c517f7cc56b435de297be92744eccf7e39
fergabilondo <68967437+fergabilondo@users.noreply.github.com>
2020-12-11 11:07:12 -0700

    11 de dic 2020

--- js/main.js
+++ js/main.js
@@ -3,3 +3,22 @@ const button = document.querySelector("button");
 console.log("funciona");
 console.log(menu);
 console.log(button);
+
+const nav = document.querySelector(".menu");
+
+function Menu(nav) {
+  const menuLink = nav.querySelector(".menu-item a");
+  const subMenu = nav.querySelector(".sub-menu");
+
+  menuLink.addEventListener("click", showSubMenu);
+
+  function showSubMenu(e) {
+    const valor = e.target;
+    console.log(valor);
+    e.preventDefault();
+    subMenu.classList.toggle("open");
+    e.stopPropagation();
+  }
+}
+
+Menu(nav);
